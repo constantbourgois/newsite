c0bac62f6e23e0f2fbc0336726f0ee291a19bac7
constantbourgois <constantbourgois@gmail.com>
2025-10-08 17:34:10 +0200

    addmetaog

--- index.php
+++ index.php
@@ -11,14 +11,29 @@
         rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="img/avatar.png" type="image/x-icon" sizes="16x16">
+    <!-- Open Graph / Facebook / Slack -->
+    <meta property="og:title" content="Constant Bourgois – Développeur Web">
+    <meta property="og:description" content="Création de sites web sur mesure, ingénierie et design moderne.">
+    <meta property="og:image" content="https://constantbourgois.com/img/avatar.png">
+    <meta property="og:url" content="https://constantbourgois.com">
+    <meta property="og:type" content="website">
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Constant Bourgois – Développeur Web">
+    <meta name="twitter:description" content="Création de sites web sur mesure, ingénierie et design moderne.">
+    <meta name="twitter:image" content="https://constantbourgois.com/img/avatar.png">
+
     <script src="scripts.js"></script>
 </head>
+
 <body>
     <?php require('pages/header.php'); ?>
     <main>
-	<?php require('pages/home.php');?>
+        <?php require('pages/home.php'); ?>
     </main>
-    <?php require('pages/footer.php');?>
-    
+    <?php require('pages/footer.php'); ?>
+
 </body>
+
 </html>
